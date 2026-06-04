e9c72af6b3a1548985e54c1fef651cf152fa5c3a
Paulo Vitor <paulovitoralves2006@gmail.com>
2026-06-04 18:16:01 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-// TODO: conectar ao banco de dados
-
-function getEngajamento(req, res) {
-  // TODO: buscar dados de engajamento das empresas no banco
-  res.json([]);
-}
-
-module.exports = { getEngajamento };
